0e10fadeb6469bbdccad6363816aa9f1b5aebf01
Isis98 <isisdaza.98@outlook.com>
2023-10-03 18:47:43 -0500

    We started to work in main.js, view.js and dataFunctions.js

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
 
+
+
+channel.addEventListener("change", selectChannel);
+
+
 console.log(filterData(data, null, null), renderItems(null), data);
--- src/view.js
+++ src/view.js
@@ -1,6 +1,15 @@
+import { filterData } from "./dataFunctions";
+
 export const renderItems = (data) => {
   console.log(data)
   // Aquí comienza tu código y puedes retornar lo que tu necesites
-  return 'example';
+  const channel = document.querySelector("select[name='channel']");
+
+  function selectChannel () {
+
+  const selectedChannel = dataFunctions.filterData(channel.value);
+  document.getElementById("root").innerHTML = JSON.stringify(selectedChannel);
+  }
+  selectChannel ();
 };
 

d35442a658cfaa892b7717ff0d094cb7501a1054
Isis98 <isisdaza.98@outlook.com>
2023-10-09 15:27:02 -0500

    Card hover with the whole information

--- src/main.js
+++ src/main.js
@@ -3,53 +3,66 @@ import { renderItems } from "./view.js";
 
 import data from "./data/dataset.js";
 
-const channel = document.querySelector("select[name='channel']");
-
 renderItems(data);
 
-channel.addEventListener("change", selectChannel);
+//Variable que almacena los filtros seleccionados y crean un objeto a la vez.
+const filters = {};
 
-function selectChannel() {
+const channel = document.querySelector("select[name='channel']");
+const audience = document.querySelector("select[name='targetAudience']");
+const status = document.querySelector("select[name='status']");
+
+
+
+channel.addEventListener("change", applyFilters);
+audience.addEventListener("change", applyFilters);
+status.addEventListener("change", applyFilters);
+
+
+function applyFilters() {
+
+    //almacenar los valores seleccionados de cada filtro en filters{}
+  filters.channel = channel.value;
+  filters.audience = audience.value;
+  filters.status = status.value;
+
+// Recorrer el objeto filters
+  filters.forEach((element) => {
+    //filtrar los elementos por los selectores escogidos
     
-  const selectedChannel = channel.value;
-
-  const filteredElements = filterData(data, "channel", selectedChannel);
+  });
 
   renderItems(filteredElements);
   
 };
 
 
-const audience = document.querySelector("select[name='targetAudience']");
 
-audience.addEventListener("change", selectPublic);
 
-function selectPublic() {
+// function selectPublic() {
 
-    const selectedPublic = audience.value;
+//     const selectedPublic = audience.value;
 
-    const filteredElements = filterData (data, "targetAudience", selectedPublic );
+//     const filteredElements = filterData (data, "targetAudience", selectedPublic );
 
-    renderItems(filteredElements)
-}
+//     renderItems(filteredElements)
+// }
 
-const transmission = document.querySelector("select[name='status']");
 
-transmission.addEventListener("change", selectTransmission);
 
-function selectTransmission() {
 
-  const selectedTransmission = transmission.value;
+// function selectTransmission() {
 
-  const filteredTransmission = filterData(data, "status", selectedTransmission);
+//   const selectedTransmission = transmission.value;
 
-  renderItems(filteredTransmission);
+//   const filteredTransmission = filterData(data, "status", selectedTransmission);
 
-};
+//   renderItems(filteredTransmission);
+
+// };
 
 // const selection = document.querySelectorAll("select[data-testid='select-filter']");
 
-// renderItems(data);
 
 // selection.addEventListener("change", selectElements);
 
@@ -57,10 +70,27 @@ function selectTransmission() {
     
 //       const selectedElements = selection.value;
     
-//       const filteredElements = filterData(data, "name" , selectedElements);
+//       const filteredElements = filterData(data, selection.name , selectedElements);
     
 //       renderItems(filteredElements);
       
 //     };
 
+//Steps: -Almacenar los selectores a filtrar
+// const selectorsList = document.querySelectorAll("select[data-testid='select-filter']");
+//[select1, select]
 
+// - Almacenar los elementos 
+{/* <select data-testid="select-filter" name="targetAudience" value=''>
+<option value="">Todos</option>
+<option value="Niños">Niños</option>
+<option value="Niños y adolescentes">Niños y adolescentes</option>
+</select> */}
+
+// -Recorrer el listado de los selectores
+//  selectorsList.forEach((element) => {
+
+//  })
+
+//Filtrar por cada selector(forEach)
+//Renderizar los elementos filtrados
--- src/view.js
+++ src/view.js
@@ -9,15 +9,36 @@ export const renderItems = (data) => {
     // const li = document.createElement("li");
     // //Inyectar cada li con los datos de cada elemento
     const li = `
+    
     <li class="elements">
+    <section class="card">
+
+    <section class="front">
       <dl itemscope itemtype="https://schema.org/TVSeries" class="user-name">
         <figure>
           <img src="${element.imageUrl}" class="img"/>
         </figure>
         <dt itemprop=""><b>${element.name}</b></dt><dd itemprop=""> Canal de Television : ${element.channel}</dd>
         <dt itemprop="">Genero: ${element.genre}</dt>
+        <dt itemprop="">Transmision: ${element.status}</dt>
+        <dt itemprop="">Audiencia: ${element.targetAudience}</dt>
+        
+        </section>
+
+        <section class="back">
+        <dt itemprop=""><b>Descripcion corta:</b> ${element.shortDescription}</dt>
+        <dt itemprop=""><b>Descripcion:</b> ${element.description}</dt>
+        <dt itemprop=""><b>Hechos importantes:</b> </dt>
+        <dd>Fecha de Creacion: ${element.facts.fechaDeCreacionDelPrograma}</dd>
+        <dd>Capitulo con mayor rating: ${element.facts.capituloConMayorRating}</dd>
+        <dd>Dato curioso: ${element.facts.datoCurioso}</dd>
+
+        </section>
       </dl>
+
+      </section>
     </li>
+    
   `;
 
     //Cada li inyectarlo en la ul

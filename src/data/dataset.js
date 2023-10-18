f6dce591a79b64712581cb8457d709226fdc9bc5
Sara Saldarriaga <prueba@correo.com>
2023-10-18 09:35:29 -0500

    Modifique el view y estyle

--- src/data/dataset.js
+++ src/data/dataset.js
@@ -41,7 +41,7 @@ export default [
     "name": "Abeja Xylocopa violacea",
     "shortDescription": "Abeja carpintera violeta",
     "description": "La Abeja Xylocopa violacea, también conocida como abeja carpintera violeta, es una abeja carpintera grande que se encuentra en Europa y otras regiones. Son conocidas por su coloración violeta y su capacidad para excavar galerías en la madera.",
-    "imageUrl": "https://www.bing.com/images/create/https3a2f2fwwwbiodiversidadvirtualorg2finsectarium2fd/65260e75f64442d083ef86ace6f77f73?id=ezRDqtp%2fvpLncE3LsqVMgw%3d%3d&view=detailv2&idpp=genimg&FORM=GCRIDP&mode=overlay",
+    "imageUrl": "https://th.bing.com/th/id/OIG.3x0WMPs6PnAshPHSWzlC?pid=ImgGn",
     "facts": {
       "promedioDeVida": "de 1 a 2 meses",
       "familia": "Apidae",
@@ -77,7 +77,7 @@ export default [
   "name": "Abeja Megachile rotundata",
   "shortDescription": "Abeja cortadora de hojas",
   "description": "La Abeja Megachile rotundata, también llamada abeja cortadora de hojas, es una especie de abeja solitaria originaria de Europa, pero ampliamente utilizada en la polinización de cultivos, especialmente alfalfa y otras plantas forrajeras.",
-  "imageUrl": "https://www.bing.com/images/create/megachile-rotundata/6525e436ba3d412ab8cf816251e5c6b1?id=zdVIzngjfSDDgBLhScmZEw%3d%3d&view=detailv2&idpp=genimg&FORM=GCRIDP&mode=overlay",
+  "imageUrl":"https://th.bing.com/th/id/OIG.H6lY3MKgU4klb3Baibq4?pid=ImgGn",
   "facts": {
     "promedioDeVida": "de 6 a 8 semanas",
     "familia": "Megachilidae",
@@ -95,7 +95,7 @@ export default [
     "name": "Abeja Osmia bicornis",
     "shortDescription": "Abeja albañil azul",
     "description": "La Abeja Osmia bicornis, conocida como abeja albañil azul, es una abeja solitaria que se encuentra en Europa y otras regiones. Son llamadas albañiles azules debido a su hábito de sellar sus nidos con barro.",
-    "imageUrl": "https://www.bing.com/images/create/osmia-bicornis/6525e48a6b9749faa2eeae29f749146a?id=OA3HCY%2burtmplMVrctNIyg%3d%3d&view=detailv2&idpp=genimg&FORM=GCRIDP&mode=overlay",
+    "imageUrl": "https://th.bing.com/th/id/OIG.gR9uYMc1cCmT4yUcOXP6?pid=ImgGn",
     "facts": {
       "promedioDeVida": "de 4 a 6 semanas",
       "familia": "Megachilidae",
@@ -113,7 +113,7 @@ export default [
   "name": "Abeja Anthidium manicatum",
   "shortDescription": "Abeja cardera",
   "description": "La Abeja Anthidium manicatum, o abeja cardera, es una especie de abeja solitaria que se encuentra en Europa y otras regiones. Son conocidas por su comportamiento de recolección de fibras vegetales para construir sus nidos.",
-  "imageUrl": "https://www.bing.com/images/create/anthidium-manicatum/6526112409a9477194e19dccd2fac1d3?id=k0hRP5epS0F0VCiMP112uA%3d%3d&view=detailv2&idpp=genimg&FORM=GCRIDP&mode=overlay",
+  "imageUrl": "https://th.bing.com/th/id/OIG.iTRt5yh2lqWhGCOnRsuS?pid=ImgGn",
   "facts": {
     "promedioDeVida": "de 4 a 6 semanas",
     "familia": "Megachilidae",
@@ -131,7 +131,7 @@ export default [
     "name": "Abeja Eucera nigrescens",
     "shortDescription": "Abeja de arena negra",
     "description": "La Abeja Eucera nigrescens, conocida como abeja de arena negra, es una especie de abeja que se encuentra en varias regiones de Europa. Son llamadas así por su preferencia por los hábitats de arena.",
-    "imageUrl": "https://www.bing.com/images/create/eucera-nigrescens/65261389912141b8b952121b7aea2270?id=hen0K9xiOYTNhQpSR3Zetw%3d%3d&view=detailv2&idpp=genimg&FORM=GCRIDP&mode=overlay",
+    "imageUrl": "https://th.bing.com/th/id/OIG.qJZNmIaU2mMqeKulERNf?pid=ImgGn",
     "facts": {
       "promedioDeVida": "de 4 a 6 semanas",
       "familia": "Apidae",
@@ -149,7 +149,7 @@ export default [
     "name": "Abeja Melipona beecheii",
     "shortDescription": "Abeja sin aguijón de la miel",
     "description": "La Abeja Melipona beecheii, también conocida como abeja sin aguijón de la miel, es una especie de abeja nativa de América Central y partes de Sudamérica. Es apreciada por la producción de miel y su comportamiento social.",
-    "imageUrl": "https://www.bing.com/images/create/melipona-beecheii/65261449118248bc961572b75eb110c7?id=QF8Ry14M3Js7g%2b9sYC5aCQ%3d%3d&view=detailv2&idpp=genimg&FORM=GCRIDP&mode=overlay",
+    "imageUrl": "https://th.bing.com/th/id/OIG.r67QZB6snAiw5btRZHR2?pid=ImgGn",
     "facts": {
       "promedioDeVida": "alrededor de 3 meses",
       "familia": "Apidae",
@@ -167,7 +167,7 @@ export default [
   "name": "Abeja Ceratina dallatorreana",
   "shortDescription": "Abeja cuerno negro",
   "description": "La Abeja Ceratina dallatorreana, o abeja cuerno negro, es una especie de abeja solitaria que se encuentra en varias regiones del mundo. Se caracteriza por su cuerpo oscuro y sus antenas en forma de cuerno.",
-  "imageUrl": "https://www.bing.com/images/create/ceratina-dallatorreana/652614e982e440ed8d951092b7069c17?id=J48yav38AeA7LnmgQVtoeg%3d%3d&view=detailv2&idpp=genimg&FORM=GCRIDP&mode=overlay",
+  "imageUrl": "https://th.bing.com/th/id/OIG.zGcT3ZnLEhOowmGG_mHE?pid=ImgGn",
   "facts": {
     "promedioDeVida": "aproximadamente 4 semanas",
     "familia": "Apidae",
@@ -185,7 +185,7 @@ export default [
     "name": "Abeja Colletes cunicularius",
     "shortDescription": "Abeja excavadora",
     "description": "La Abeja Colletes cunicularius, conocida como abeja excavadora, es una especie de abeja solitaria que se encuentra en diversas partes de Europa. Son llamadas así por su hábito de excavar nidos en el suelo.",
-    "imageUrl": "https://www.bing.com/images/create/colletes-cunicularius/65261598351c4facb6d4cd9f9ee55550?id=0z31vU30eF2b8fF9zZzwlw%3d%3d&view=detailv2&idpp=genimg&FORM=GCRIDP&mode=overlay",
+    "imageUrl": "https://th.bing.com/th/id/OIG.1L0M4524YJpeqLBdD55P?pid=ImgGn",
     "facts": {
       "promedioDeVida": "alrededor de 4 semanas",
       "familia": "Colletidae",
@@ -203,7 +203,7 @@ export default [
   "name": "Abeja Hylaeus hyalinatus",
   "shortDescription": "Abeja de vidrio",
   "description": "La Abeja Hylaeus hyalinatus, conocida como abeja de vidrio, es una especie de abeja solitaria que se encuentra en varias regiones de Europa. Se llama así debido a su apariencia transparente.",
-  "imageUrl": "https://www.bing.com/images/create/hylaeus-hyalinatus/6526160e6b8e4edc9520e21ca6000ac7?id=jfRBMKu4tiGqBBKojEaR1g%3d%3d&view=detailv2&idpp=genimg&FORM=GCRIDP&mode=overlay",
+  "imageUrl": "https://th.bing.com/th/id/OIG.PpHrmL59.b1VACodupjq?pid=ImgGn",
   "facts": {
     "promedioDeVida": "de 2 a 4 semanas",
     "familia": "Colletidae",
@@ -221,7 +221,7 @@ export default [
   "name": "Abeja Lasioglossum calceatum",
   "shortDescription": "Abeja peluda",
   "description": "La Abeja Lasioglossum calceatum, conocida como abeja peluda, es una especie de abeja solitaria que se encuentra en varias regiones de Europa. Se caracteriza por su cuerpo cubierto de pelos.",
-  "imageUrl": "https://www.bing.com/images/create/lasioglossum-calceatum/65261673ab564a69a4c8728246db6d76?id=Dtn5aT0RC7MSVrlwlyLTgg%3d%3d&view=detailv2&idpp=genimg&FORM=GCRIDP&mode=overlay",
+  "imageUrl": "https://th.bing.com/th/id/OIG.gqcu5z4yhaGUoRZiNV9k?pid=ImgGn",
   "facts": {
     "promedioDeVida": "alrededor de 4 semanas",
     "familia": "Halictidae",
@@ -239,7 +239,7 @@ export default [
     "name": "Abeja Nomia melanderi",
     "shortDescription": "Abeja de tierra metálica",
     "description": "La Abeja Nomia melanderi, conocida como abeja de tierra metálica, es una especie de abeja que se encuentra en América del Norte. Su nombre se debe a su apariencia metálica y su preferencia por hábitats terrestres.",
-    "imageUrl": "https://www.bing.com/images/create/22abeja-nomia-melanderi222c/652616e1972e4b5c9450f6e6b4ef30f9?id=FPIIJUJfJwHyX48YwDNdpA%3d%3d&view=detailv2&idpp=genimg&FORM=GCRIDP&mode=overlay",
+    "imageUrl": "https://th.bing.com/th/id/OIG.1eqkqZgmNMI26Jmak0hn?pid=ImgGn",
     "facts": {
       "promedioDeVida": "alrededor de 4 semanas",
       "familia": "Halictidae",
@@ -257,7 +257,7 @@ export default [
   "name": "Abeja Andrena wilkella",
   "shortDescription": "Abeja minera",
   "description": "La Abeja Andrena wilkella, también conocida como abeja minera, es una especie de abeja que se encuentra en varias partes del mundo. Son llamadas abejas mineras debido a su comportamiento de anidar en el suelo.",
-  "imageUrl": "https://www.bing.com/images/create/abeja-andrena-wilkella/652618ed3ee441568630558c34a5ca39?id=g1OQM%2fQMBNtrJCxHCzZxmA%3d%3d&view=detailv2&idpp=genimg&FORM=GCRIDP&mode=overlay",
+  "imageUrl": "https://th.bing.com/th/id/OIG.3OrxJZgKk.RcUJVzJUvL?pid=ImgGn",
   "facts": {
     "promedioDeVida": "de 3 a 5 semanas",
     "familia": "Andrenidae",
@@ -275,7 +275,7 @@ export default [
     "name": "Abeja Perdita minima",
     "shortDescription": "Abeja perdita",
     "description": "La Abeja Perdita minima, conocida como abeja perdita, es una pequeña especie de abeja que se encuentra en América del Norte. Su nombre se debe a su tamaño diminuto.",
-    "imageUrl": "https://www.bing.com/images/create/https3a2f2fmediaistockphotocom2fid2f11734903382fes2ffot/65261b9735074751b879ff00ab4fc10e?id=PXuxNPjm5kwPYpieA7DHYQ%3d%3d&view=detailv2&idpp=genimg&FORM=GCRIDP&mode=overlay",
+    "imageUrl": "https://th.bing.com/th/id/OIG.VazgZ0xT9rlZg4nVtlub?pid=ImgGn",
     "facts": {
       "promedioDeVida": "alrededor de 3 semanas",
       "familia": "Andrenidae",
@@ -293,7 +293,7 @@ export default [
     "name": "Abeja Epeolus variegatus",
     "shortDescription": "Abeja cleptoparásita",
     "description": "La Abeja Epeolus variegatus, conocida como abeja cleptoparásita, es una especie de abeja que se encuentra en diversas partes del mundo. Son cleptoparásitas, lo que significa que depositan sus huevos en los nidos de otras abejas.",
-    "imageUrl": "https://www.bing.com/images/create/https3a2f2fbwarscom2fsites2fdefault2ffiles2fspeciesimag/65261ce9007c458fb4314fb2ad7a4ae5?id=fnQ03Eh0Cx4JBXk4dwK5EQ%3d%3d&view=detailv2&idpp=genimg&FORM=GCRIDP&mode=overlay",
+    "imageUrl": "https://th.bing.com/th/id/OIG.49idOkEGaBOdOmjU903V?pid=ImgGn",
     "facts": {
       "promedioDeVida": "alrededor de 3 semanas",
       "familia": "Apidae",
@@ -311,7 +311,7 @@ export default [
     "name": "Abeja Agapostemon texanus",
     "shortDescription": "Abeja rayada verde",
     "description": "La Abeja Agapostemon texanus, conocida como abeja rayada verde, es una especie de abeja que se encuentra en América del Norte. Son reconocibles por sus rayas verdes y negras en el abdomen.",
-    "imageUrl": "https://www.bing.com/images/create/agapostemon-texanus/65261dc9cfc34b9dbc513c0836a4fdde?id=SsahTZ%2brKUPikBQdxKFt6w%3d%3d&view=detailv2&idpp=genimg&FORM=GCRIDP&mode=overlay",
+    "imageUrl": "https://th.bing.com/th/id/OIG.7FgYcS.uYxQYEa3.KEC4?pid=ImgGn",
     "facts": {
       "promedioDeVida": "de 3 a 4 semanas",
       "familia": "Halictidae",
@@ -329,7 +329,7 @@ export default [
     "name": "Abeja Dialictus zephyrus",
     "shortDescription": "Abeja esmeralda",
     "description": "La Abeja Dialictus zephyrus, conocida como abeja esmeralda, es una especie de abeja que se encuentra en América del Norte. Su nombre se debe a su coloración verde esmeralda.",
-    "imageUrl": "https://www.bing.com/images/create/dialictus-zephyrus/65261e34164540c1abb552a335faec0a?id=b3B1d28pI06U9F0ab7DOaw%3d%3d&view=detailv2&idpp=genimg&FORM=GCRIDP&mode=overlay",
+    "imageUrl": "https://th.bing.com/th/id/OIG.0f90dz58tmzddR9u75qI?pid=ImgGn",
     "facts": {
       "promedioDeVida": "de 3 a 4 semanas",
       "familia": "Halictidae",
@@ -347,7 +347,7 @@ export default [
   "name": "Abeja Megachile sculpturalis",
   "shortDescription": "Abeja cortadora de hojas gigante",
   "description": "La Abeja Megachile sculpturalis, también conocida como abeja cortadora de hojas gigante, es una especie de abeja que se encuentra en varias partes del mundo. Son conocidas por su gran tamaño y capacidad para cortar hojas para construir sus nidos.",
-  "imageUrl": "https://www.bing.com/images/create/megachile-sculpturalis/65262365c97c40c7b1fa0dde773ccd6a?id=NN8XPLV6t7wxbPrNwFZsBg%3d%3d&view=detailv2&idpp=genimg&FORM=GCRIDP&mode=overlay",
+  "imageUrl": "https://th.bing.com/th/id/OIG.1AENiIX50BhvXQPYnA4l?pid=ImgGn",
   "facts": {
     "promedioDeVida": "de 4 a 6 semanas",
     "familia": "Megachilidae",
@@ -365,7 +365,7 @@ export default [
     "name": "Abeja Nomada fervida",
     "shortDescription": "Abeja cleptoparásita",
     "description": "La Abeja Nomada fervida, conocida como abeja cleptoparásita, es una especie de abeja que se encuentra en América del Norte. Son cleptoparásitas que depositan sus huevos en los nidos de otras abejas solitarias.",
-    "imageUrl": "https://www.bing.com/images/create/abeja-nomada-fervida/6526a9aa00d24762aa225233d4fff8c1?id=7W8zz7ZqnnyUiKxnhYDosg%3d%3d&view=detailv2&idpp=genimg&FORM=GCRIDP&mode=overlay",
+    "imageUrl": "https://th.bing.com/th/id/OIG.VazgZ0xT9rlZg4nVtlub?pid=ImgGn",
     "facts": {
       "promedioDeVida": "alrededor de 4 semanas",
       "familia": "Apidae",
@@ -383,7 +383,7 @@ export default [
     "name": "Abeja Hylaeus signatus",
     "shortDescription": "Abeja de cara amarilla",
     "description": "La Abeja Hylaeus signatus, conocida como abeja de cara amarilla, es una especie de abeja que se encuentra en América del Norte. Su nombre se debe a su característica cara amarilla.",
-    "imageUrl": "URL_IMAGEN_HYLAEUS_SIGNATUS",
+    "imageUrl": "https://th.bing.com/th/id/OIG.gGDB2nCA7YkKNloc07G7?w=270&h=270&c=6&r=0&o=5&pid=ImgGn",
     "facts": {
       "promedioDeVida": "de 2 a 4 semanas",
       "familia": "Colletidae",
@@ -401,7 +401,7 @@ export default [
   "name": "Abeja Bombus impatiens",
   "shortDescription": "Abejorro impaciente",
   "description": "El Abejorro Bombus impatiens, conocido como abejorro impaciente, es una especie de abejorro que se encuentra en América del Norte. Son polinizadores importantes en entornos naturales y agrícolas.",
-  "imageUrl": "URL_IMAGEN_BOMBUS_IMPATIENS",
+  "imageUrl": "https://th.bing.com/th/id/OIG.Qpehm6.T9I8no4fZe6Ml?w=270&h=270&c=6&r=0&o=5&pid=ImgGn",
   "facts": {
     "promedioDeVida": "de 12 a 16 semanas",
     "familia": "Apidae",
@@ -419,7 +419,7 @@ export default [
     "name": "Abeja Osmia lignaria",
     "shortDescription": "Abeja albañil azul",
     "description": "La Abeja Osmia lignaria, conocida como abeja albañil azul, es una especie de abeja solitaria que se encuentra en América del Norte. Son llamadas albañiles azules debido a su hábito de sellar sus nidos con barro.",
-    "imageUrl": "URL_IMAGEN_OSMIA_LIGNARIA",
+    "imageUrl": "https://th.bing.com/th/id/OIG.GWBxCH5wZZGJhsoeV9Zk?w=270&h=270&c=6&r=0&o=5&pid=ImgGn",
     "facts": {
       "promedioDeVida": "de 4 a 6 semanas",
       "familia": "Megachilidae",
